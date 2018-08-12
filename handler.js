81f8a0a22c381f55d8e491dbf6f857389c7c5974
John Dawes <jdawes@bluebitmedia.com>
2018-08-12 11:20:33 -0600

    Emoji rank

--- handler.js
+++ handler.js
@@ -1,16 +1,24 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+// Rank by emoji
+const emojis = ['🧡', '💛', '💚', '💙', '💜', '💗', '💖', '💘', '💝'];
 
-  callback(null, response);
+module.exports.rank = (event, context, callback) => {
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    const rank = event.queryStringParameters.rank;
+
+    const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            input: rankEmoji, // event - to see all the possiblities
+        }),
+    };
+
+    callback(null, response);
+
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
